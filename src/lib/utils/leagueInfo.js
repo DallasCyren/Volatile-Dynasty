145a2503b5b76d29ca0fb7a70dfe5bdb5086abc1
DallasCyren <dennislmitchell@gmail.com>
2025-05-19 17:05:51 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -24,10 +24,10 @@ export const managers = [
       "managerID": "1068431841838841856",
       "name": "Dennis",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "Brooklyn", // (optional)
-      "bio": "Lorem ipsum...",
+      "location": "Dallas, GA", // (optional)
+      "bio": "Retired, bored and looking for things to do like this to keep me young",
       "photo": "/managers/Dennis.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "atl", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
