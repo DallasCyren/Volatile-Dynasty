0eb3bc765b582a3f76c3af266e2a178c0912bedf
Dennis Mitchell <dennislmitchell@gmail.com>
2025-05-27 07:46:42 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,4 +1,15 @@
+<script>
+    import { dues } from '$lib/utils/helper';
+    let one, oneOne, oneOneOne, oneTwo, oneTwoOne, oneTwoTwo, oneTwoThree, oneTwoFour, oneTwoFive, oneThree;
+    let two, twoOne, twoTwo, twoThree;
+    let three, threeOne;
+    let four, fourOne, fourTwo;
+    let five, fiveOne;
+    let six, sixOne, sixTwo, sixThree;
+    let seven, sevenOne, sevenTwo, sevenThree;
+
 <div class="constitution">
+
 {
         position: relative;
         z-index: 1;
